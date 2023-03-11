90d1a788db284cadef2c25b3fdeb33acbe833e37
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-03-10 20:50:27 -0500

    Update LibraryRecord.hpp

--- LibraryRecord.hpp
+++ LibraryRecord.hpp
@@ -1,9 +1,8 @@
-#ifndef LIBRARYRECORD_H
-#define LIBRARYRECORD_H
+#ifndef LIBRARYRECORD_HPP_
+#define LIBRARYRECORD_HPP_
+
 #include "ArrayBag.hpp"
 #include "Book.hpp"
-#include <iostream>
-#include <vector>
 
 class LibraryRecord : public ArrayBag<Book>{
     /*- A vector containing a copy of all the Books that have been checked out.*/
@@ -12,16 +11,17 @@ class LibraryRecord : public ArrayBag<Book>{
 
     public:
     LibraryRecord();
-    bool checkIn(const Book& checkin_);
+    bool checkIn(const Book& checkin_); 
     bool checkOut(const Book& checkout_);
     int getCheckOutHistory(const Book& History_) const;
-    void display() const;
-    void displayTitles() const;
+    void display() const; 
+    void displayTitles() const; 
     bool duplicateStock();
-    bool removeStock(const Book& removestock_);
-    bool equivalentRecords(const LibraryRecord& LibraryRecord_) const;
+    bool removeStock(const Book& removestock_); 
+    bool equivalentRecords(const LibraryRecord& LibraryRecord_) const; 
     LibraryRecord& operator+=(const LibraryRecord& duplicate_add);
     LibraryRecord& operator/=(const LibraryRecord& non_duplicate_add);
+
 };
 
 #endif
